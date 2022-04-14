49f7989e9835950e92f3ed00d8a1a2dbbc23ad34
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-14 22:41:27 +0200

    chore: everything works

--- src/components/GradientText.tsx
+++ src/components/GradientText.tsx
@@ -1,17 +1,11 @@
-import PropTypes from 'prop-types'
-
 type Props = {
-  children: any
-}
+  children: any;
+};
 
 export default function GradientText({ children }: Props): JSX.Element {
   return (
     <span className="-mx-2 px-2 bg-clip-text text-transparent bg-gradient-to-r from-primary-500 to-secondary-500">
       {children}
     </span>
-  )
-}
-
-GradientText.propTypes = {
-  children: PropTypes.node.isRequired,
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 const path = require("path");
+// eslint-disable-next-line import/no-unresolved
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [react(), Icons({ compiler: 'jsx', jsx: 'react' })],
 });
