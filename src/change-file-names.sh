3db467a6b8d7f6f865becbf3cb188c2e3aae8b75
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-14 22:39:15 +0200

    chore: change all jsx to tsx

--- src/change-file-names.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find . -name "123*.txt" -exec rename 's/^123_//' {} ";"
-
-find . -name '*.jsx' -type f -exec sh -c '
-for f; do
-    echo $f
-    echo "${f%/*}/${f##*/123_}"
-done' sh {} +
